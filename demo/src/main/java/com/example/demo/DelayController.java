2e2877638188e96f351672a84120f5ac8d7d46d6
Daniel Ribeiro <daniel.junior@gmail.com>
2025-03-01 10:29:53 -0300

    Update

--- demo/src/main/java/com/example/demo/DelayController.java
+++ demo/src/main/java/com/example/demo/DelayController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +16,15 @@ public class DelayController {
     }
 
     @GetMapping("/simulate-delay")
-    public String simulateDelay() throws InterruptedException {
-        // Simula um delay de 10 segundos
-        Thread.sleep(10000);
-        return "Resposta após 10 segundos!";
+    public ResponseEntity<String> simulateDelay() {
+        try {
+            // Cria um objeto grande que não será coletado pelo GC
+            byte[] memoria = new byte[1024 * 1024 * 10]; // 100MB
+
+            return ResponseEntity.ok().body("ok");
+        } catch (Exception e) {
+            Thread.currentThread().interrupt(); // Restore interrupted status
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
     }
 }
---
+++ reverse-proxy/README.md
@@ -0,0 +1,7 @@
+
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_balancer
+sudo a2enmod proxy_ajp
+sudo a2enmod proxy_wstunnel
+sudo a2enmod ratelimit
